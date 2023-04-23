24e234145b02ab350793c5e50d27b61f9364541a
hyunhaha <gusgk2658@naver.com>
2023-04-23 17:15:48 +0900

    아이콘 추가

--- src/pages/store/Detail.tsx
+++ src/pages/store/Detail.tsx
@@ -4,8 +4,14 @@ import { Box, styled } from '@mui/system';
 import PlaceIcon from '@mui/icons-material/Place';
 import Typography from '@mui/material/Typography';
 import CallIcon from '@mui/icons-material/Call';
-import { List, ListItem, ListItemText } from '@mui/material';
+import { List, ListItem, ListItemAvatar, ListItemText } from '@mui/material';
 import MenuAppBar from '../../Components/MenuAppBar';
+import AccessibleIcon from '@mui/icons-material/Accessible';
+import WifiIcon from '@mui/icons-material/Wifi';
+import PetsIcon from '@mui/icons-material/Pets';
+import LocalParkingIcon from '@mui/icons-material/LocalParking';
+import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
 interface DetailProps {}
 
 const Detail: FC<DetailProps> = () => {
@@ -32,7 +38,7 @@ const Detail: FC<DetailProps> = () => {
           <Box display={'flex'} width="100%" alignItems={'center'} py={1}>
             <PlaceIcon />
             <Typography variant="body1" sx={{ pl: 1 }}>
-              글자가 길면 자동으로 다음 줄로 넘어갑니다.
+              글자가 길면 자동으로 다음 줄로 넘어갑니다.글자가 길면 자동으로 다음 줄로 넘어갑니다.
             </Typography>
           </Box>
           <Box display={'flex'} width="100%" alignItems={'center'} py={1}>
@@ -60,10 +66,36 @@ const Detail: FC<DetailProps> = () => {
               상세 정보
             </Typography>
             <div>
-              <List dense={true}>
-                {['포장가능'].map((e, index) => (
-                  <ListItem key={index}>
-                    <ListItemText primary={e} />
+              <List
+                dense={true}
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  flexWrap: 'wrap',
+                  width: '100%',
+                }}
+              >
+                {[
+                  { title: '포장가능', icon: <CardGiftcardIcon /> },
+                  { title: 'WiFi', icon: <WifiIcon /> },
+                  { title: '동물출입', icon: <PetsIcon /> },
+                  { title: '주차', icon: <LocalParkingIcon /> },
+                  { title: '휠체어사용', icon: <AccessibleIcon /> },
+                  { title: '놀이방', icon: <SportsEsportsIcon /> },
+                ].map((e, index) => (
+                  <ListItem
+                    key={index}
+                    sx={{
+                      paddingLeft: '4px',
+                      paddingRight: '4px',
+                      width: '20%',
+                      display: 'flex',
+                      flexDirection: 'column',
+                      textAlign: 'center',
+                    }}
+                  >
+                    <ListItemAvatar>{e.icon}</ListItemAvatar>
+                    <ListItemText primary={e.title} />
                   </ListItem>
                 ))}
               </List>
